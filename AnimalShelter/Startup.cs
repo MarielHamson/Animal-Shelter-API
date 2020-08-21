0cf949ca84ffa465704270cb2b28aaa983e81c06
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-08-21 12:50:31 -0700

    remove JWT

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -27,22 +27,6 @@ namespace AnimalShelter
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      // services.AddCors(options =>
-      // {
-      //   options.AddDefaultPolicy(
-      //         builder =>
-      //         {
-      //           builder.WithOrigins("http://example.com", "https://localhost:5001");
-      //         });
-
-      // });
-
-      // services.Configure<CookiePolicyOptions>(options =>
-      // {
-      //   // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-      //   options.CheckConsentNeeded = context => true;
-      //   options.MinimumSameSitePolicy = SameSiteMode.None;
-      // });
 
       services.AddDbContext<AnimalShelterContext>(opt =>
           opt.UseMySql(Configuration.GetConnectionString("DefaultConnection"),
@@ -59,33 +43,6 @@ namespace AnimalShelter
         return new UriService(uri);
       });
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-      // var appSettingsSection = Configuration.GetSection("AppSettings");
-      // services.Configure<AppSettings>(appSettingsSection);
-
-      // configure jwt authentication
-      // var appSettings = appSettingsSection.Get<AppSettings>();
-      // var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-      // services.AddAuthentication(x =>
-      // {
-      //   x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-      //   x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-      // })
-      // .AddJwtBearer(x =>
-      // {
-      //   x.RequireHttpsMetadata = false;
-      //   x.SaveToken = true;
-      //   x.TokenValidationParameters = new TokenValidationParameters
-      //   {
-      //     ValidateIssuerSigningKey = true,
-      //     IssuerSigningKey = new SymmetricSecurityKey(key),
-      //     ValidateIssuer = false,
-      //     ValidateAudience = false
-      //   };
-      // });
-
-      // configure DI for application services
-      services.AddScoped<IUserService, UserService>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -98,7 +55,6 @@ namespace AnimalShelter
       c.RoutePrefix = string.Empty;
 
     });
-      // app.UseAuthentication();
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
@@ -110,7 +66,6 @@ namespace AnimalShelter
         app.UseHsts();
       }
 
-      // app.UseCors();
       // app.UseHttpsRedirection();
       app.UseMvc();
     }
