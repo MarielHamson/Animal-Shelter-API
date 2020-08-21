358127b59547a3b1e700affa9c0b3e4a9bd622ab
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-08-21 14:38:52 -0700

    reseed database

---
+++ AnimalShelter/Migrations/20200821212758_ReSeedDB.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class ReSeedDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "GetsAlongWith" },
+                values: new object[] { "4", "Labrador Retriever", "Black", "super sweet but bitey", "Small dogs" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "GetsAlongWith", "NumOfFeet", "Type" },
+                values: new object[] { "7", "Iguana", "speckled", "lays around", "Handler", 3f, "Lizard" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4,
+                columns: new[] { "Breed", "Coloring", "GetsAlongWith" },
+                values: new object[] { "Pug", "Beige with Black muzzle", "big dogs, friends" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "GetsAlongWith", "Type" },
+                values: new object[] { "6 months", "Potbelly", "pink with black spots", "wants cuddles forever", "Children", "Pig" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "Type" },
+                values: new object[] { "3", "Leopard", "Yellow with spots", "rough and tumble", "Cat" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7,
+                columns: new[] { "Breed", "Coloring", "Disposition", "GetsAlongWith" },
+                values: new object[] { "Weimeraner", "Grey/Silver", "bouncey and happy", "Adults and random strangers" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 8,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "GetsAlongWith", "NumOfFeet", "Type" },
+                values: new object[] { "5", "Just Rat", "pink and grey", "loves to climb", "Small things", 3f, "Rat" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 9,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "GetsAlongWith" },
+                values: new object[] { "7", "Rat Terrier", "white with brown spots, or is it brown with white spots", "can be nice when food is available", "me" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 10,
+                columns: new[] { "Age", "Breed", "Disposition", "GetsAlongWith", "NumOfFeet", "Type" },
+                values: new object[] { "10", "Roo", "loyal and honest", "Everyone", 2f, "Kangaroo" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "GetsAlongWith" },
+                values: new object[] { "1", "Golden Retriever", "Yellow", "super sweet", "Small dogs, children" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "GetsAlongWith", "NumOfFeet", "Type" },
+                values: new object[] { "1", "Golden Retriever", "Yellow", "super sweet", "Small dogs, children", 4f, "Dog" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4,
+                columns: new[] { "Breed", "Coloring", "GetsAlongWith" },
+                values: new object[] { "Golden Retriever", "Yellow", "Small dogs, children" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "GetsAlongWith", "Type" },
+                values: new object[] { "1", "Golden Retriever", "Yellow", "super sweet", "Small dogs, children", "Dog" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "Type" },
+                values: new object[] { "1", "Golden Retriever", "Yellow", "super sweet", "Dog" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7,
+                columns: new[] { "Breed", "Coloring", "Disposition", "GetsAlongWith" },
+                values: new object[] { "Golden Retriever", "Yellow", "super sweet", "Small dogs, children" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 8,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "GetsAlongWith", "NumOfFeet", "Type" },
+                values: new object[] { "1", "Golden Retriever", "Yellow", "super sweet", "Small dogs, children", 4f, "Dog" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 9,
+                columns: new[] { "Age", "Breed", "Coloring", "Disposition", "GetsAlongWith" },
+                values: new object[] { "1", "Golden Retriever", "Yellow", "super sweet", "Small dogs, children" });
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 10,
+                columns: new[] { "Age", "Breed", "Disposition", "GetsAlongWith", "NumOfFeet", "Type" },
+                values: new object[] { "1", "Golden Retriever", "super sweet", "Small dogs, children", 4f, "Dog" });
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -17,15 +17,15 @@ namespace AnimalShelter.Models
       builder.Entity<Animal>()
         .HasData(
           new Animal { AnimalId = 1, EnergyLevel = "High", GetsAlongWith = "Small dogs, children", Type = "Dog", Breed = "Golden Retriever", Age = "1", Disposition = "super sweet", Coloring = "Yellow", NumOfFeet = 4, Size = "Perfect" },
-          new Animal { AnimalId = 2, EnergyLevel = "Medium", GetsAlongWith = "Small dogs, children", Type = "Dog", Breed = "Golden Retriever", Age = "1", Disposition = "super sweet", Coloring = "Yellow", NumOfFeet = 4, Size = "Perfect" },
-          new Animal { AnimalId = 3, EnergyLevel = "Low", GetsAlongWith = "Small dogs, children", Type = "Dog", Breed = "Golden Retriever", Age = "1", Disposition = "super sweet", Coloring = "Yellow", NumOfFeet = 4, Size = "Perfect" },
-          new Animal { AnimalId = 4, EnergyLevel = "Slothlike", GetsAlongWith = "Small dogs, children", Type = "Dog", Breed = "Golden Retriever", Age = "1", Disposition = "super sweet", Coloring = "Yellow", NumOfFeet = 4, Size = "Perfect" },
-          new Animal { AnimalId = 5, EnergyLevel = "Bouncy", GetsAlongWith = "Small dogs, children", Type = "Dog", Breed = "Golden Retriever", Age = "1", Disposition = "super sweet", Coloring = "Yellow", NumOfFeet = 4, Size = "Perfect" },
-          new Animal { AnimalId = 6, EnergyLevel = "Chill", GetsAlongWith = "Small dogs, children", Type = "Dog", Breed = "Golden Retriever", Age = "1", Disposition = "super sweet", Coloring = "Yellow", NumOfFeet = 4, Size = "Perfect" },
-          new Animal { AnimalId = 7, EnergyLevel = "Medium", GetsAlongWith = "Small dogs, children", Type = "Dog", Breed = "Golden Retriever", Age = "1", Disposition = "super sweet", Coloring = "Yellow", NumOfFeet = 4, Size = "Perfect" },
-          new Animal { AnimalId = 8, EnergyLevel = "Med-Low", GetsAlongWith = "Small dogs, children", Type = "Dog", Breed = "Golden Retriever", Age = "1", Disposition = "super sweet", Coloring = "Yellow", NumOfFeet = 4, Size = "Perfect" },
-          new Animal { AnimalId = 9, EnergyLevel = "Highest", GetsAlongWith = "Small dogs, children", Type = "Dog", Breed = "Golden Retriever", Age = "1", Disposition = "super sweet", Coloring = "Yellow", NumOfFeet = 4, Size = "Perfect" },
-          new Animal { AnimalId = 10, EnergyLevel = "Normal", GetsAlongWith = "Small dogs, children", Type = "Dog", Breed = "Golden Retriever", Age = "1", Disposition = "super sweet", Coloring = "Yellow", NumOfFeet = 4, Size = "Perfect" });
+          new Animal { AnimalId = 2, EnergyLevel = "Medium", GetsAlongWith = "Small dogs", Type = "Dog", Breed = "Labrador Retriever", Age = "4", Disposition = "super sweet but bitey", Coloring = "Black", NumOfFeet = 4, Size = "Perfect" },
+          new Animal { AnimalId = 3, EnergyLevel = "Low", GetsAlongWith = "Handler", Type = "Lizard", Breed = "Iguana", Age = "7", Disposition = "lays around", Coloring = "speckled", NumOfFeet = 3, Size = "Perfect" },
+          new Animal { AnimalId = 4, EnergyLevel = "Slothlike", GetsAlongWith = "big dogs, friends", Type = "Dog", Breed = "Pug", Age = "1", Disposition = "super sweet", Coloring = "Beige with Black muzzle", NumOfFeet = 4, Size = "Perfect" },
+          new Animal { AnimalId = 5, EnergyLevel = "Bouncy", GetsAlongWith = "Children", Type = "Pig", Breed = "Potbelly", Age = "6 months", Disposition = "wants cuddles forever", Coloring = "pink with black spots", NumOfFeet = 4, Size = "Perfect" },
+          new Animal { AnimalId = 6, EnergyLevel = "Chill", GetsAlongWith = "Small dogs, children", Type = "Cat", Breed = "Leopard", Age = "3", Disposition = "rough and tumble", Coloring = "Yellow with spots", NumOfFeet = 4, Size = "Perfect" },
+          new Animal { AnimalId = 7, EnergyLevel = "Medium", GetsAlongWith = "Adults and random strangers", Type = "Dog", Breed = "Weimeraner", Age = "1", Disposition = "bouncey and happy", Coloring = "Grey/Silver", NumOfFeet = 4, Size = "Perfect" },
+          new Animal { AnimalId = 8, EnergyLevel = "Med-Low", GetsAlongWith = "Small things", Type = "Rat", Breed = "Just Rat", Age = "5", Disposition = "loves to climb", Coloring = "pink and grey", NumOfFeet = 3, Size = "Perfect" },
+          new Animal { AnimalId = 9, EnergyLevel = "Highest", GetsAlongWith = "me", Type = "Dog", Breed = "Rat Terrier", Age = "7", Disposition = "can be nice when food is available", Coloring = "white with brown spots, or is it brown with white spots", NumOfFeet = 4, Size = "Perfect" },
+          new Animal { AnimalId = 10, EnergyLevel = "Normal", GetsAlongWith = "Everyone", Type = "Kangaroo", Breed = "Roo", Age = "10", Disposition = "loyal and honest", Coloring = "Yellow", NumOfFeet = 2, Size = "Perfect" });
     }
   }
 }
